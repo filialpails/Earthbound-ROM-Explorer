e5bb779ce912a52849b746d71e44cb48a7fc3f10
filialpails <bobert_1234567890@hotmail.com>
2014-07-25 02:23:12 -0400

    use default railties loading code

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
-
-%w(
-active_model
-action_controller
-action_view
-sprockets
-).each do |framework|
-  begin
-    require "#{framework}/railtie"
-  rescue LoadError
-  end
-end
+require "active_model/railtie"
+# require "active_record/railtie"
+require "action_controller/railtie"
+# require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
